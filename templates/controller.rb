0f8d19b668de22d8dcbc95ea3bde1d4a7ae0c2dc
wolfpack <wolfpack@vulk.co>
2016-03-07 21:38:30 -0500

    multi-word resource names now are snake case

--- templates/controller.rb
+++ templates/controller.rb
@@ -2,58 +2,58 @@ class <%= class_name.pluralize %>Controller < ApplicationController
   def index
     # probably should search by authtoken
     # if params[:xxx_id]
-    #   <%= class_name.downcase.pluralize %> = <%= class_name %>.where(xxx_id: params[:xxx_id])
+    #   <%= class_name.underscore.pluralize %> = <%= class_name %>.where(xxx_id: params[:xxx_id])
     # else
-      @<%= class_name.downcase.pluralize %> = <%= class_name %>.all
+      @<%= class_name.underscore.pluralize %> = <%= class_name %>.all
     # end 
-    if @<%= class_name.downcase.pluralize %>
-      render json: {:<%= class_name.downcase.pluralize %> => @<%= class_name.downcase.pluralize %>}
+    if @<%= class_name.underscore.pluralize %>
+      render json: {:<%= class_name.underscore.pluralize %> => @<%= class_name.underscore.pluralize %>}
     else
-      render json: {:errors => 'No <%= class_name.downcase.pluralize %> found'}, :status => 404
+      render json: {:errors => 'No <%= class_name.underscore.pluralize %> found'}, :status => 404
     end
   end
 
   def show
-    @<%= class_name.downcase.pluralize %> = <%= class_name %>.find(params[:id])
-    render json: {:<%= class_name.downcase.pluralize %> => @<%= class_name.downcase.pluralize %>}
+    @<%= class_name.underscore.pluralize %> = <%= class_name %>.find(params[:id])
+    render json: {:<%= class_name.underscore.pluralize %> => @<%= class_name.underscore.pluralize %>}
     
-    @<%= class_name.downcase.pluralize %> = <%= class_name %>.where(id: params[:id])
-    if !@<%= class_name.downcase.pluralize %>.empty?
-      render json: {:<%= class_name.downcase.pluralize %> => @<%= class_name.downcase.pluralize %>.first}
+    @<%= class_name.underscore.pluralize %> = <%= class_name %>.where(id: params[:id])
+    if !@<%= class_name.underscore.pluralize %>.empty?
+      render json: {:<%= class_name.underscore.pluralize %> => @<%= class_name.underscore.pluralize %>.first}
     else
       render json: {:error => '<%= class_name %> not found'}, :status => 404
     end
   end
 
   def create
-    @<%= class_name.downcase.pluralize %> = <%= class_name %>.create(<%= class_name.downcase %>_params)
-    if @<%= class_name.downcase.pluralize %>.save
-      render json: {:<%= class_name.downcase.pluralize %> => @<%= class_name.downcase.pluralize %>}
+    @<%= class_name.underscore.pluralize %> = <%= class_name %>.create(<%= class_name.downcase %>_params)
+    if @<%= class_name.underscore.pluralize %>.save
+      render json: {:<%= class_name.underscore.pluralize %> => @<%= class_name.underscore.pluralize %>}
     else
-      render :json => { :errors => @<%= class_name.downcase.pluralize %>.errors.full_messages }, :status => 422
+      render :json => { :errors => @<%= class_name.underscore.pluralize %>.errors.full_messages }, :status => 422
     end
   end
 
   def update 
-    @<%= class_name.downcase.pluralize %> = <%= class_name %>.find(params[:id])
-    if @<%= class_name.downcase.pluralize %>.update!(<%= class_name.downcase %>_params)
-      render json: {:<%= class_name.downcase.pluralize %> => @<%= class_name.downcase.pluralize %>}
+    @<%= class_name.underscore.pluralize %> = <%= class_name %>.find(params[:id])
+    if @<%= class_name.underscore.pluralize %>.update!(<%= class_name.downcase %>_params)
+      render json: {:<%= class_name.underscore.pluralize %> => @<%= class_name.underscore.pluralize %>}
     else
-      render :json => { :errors => @<%= class_name.downcase.pluralize %>.errors.full_messages }, :status => 422
+      render :json => { :errors => @<%= class_name.underscore.pluralize %>.errors.full_messages }, :status => 422
     end
   end
 
   def destroy 
-    @<%= class_name.downcase.pluralize %> = <%= class_name %>.find(params[:id])
-    if @<%= class_name.downcase.pluralize %>
-      @<%= class_name.downcase.pluralize %>.save
-      if @<%= class_name.downcase.pluralize %>.destroy!
-        render json: {:<%= class_name.downcase.pluralize %> => @<%= class_name.downcase.pluralize %>}
+    @<%= class_name.underscore.pluralize %> = <%= class_name %>.find(params[:id])
+    if @<%= class_name.underscore.pluralize %>
+      @<%= class_name.underscore.pluralize %>.save
+      if @<%= class_name.underscore.pluralize %>.destroy!
+        render json: {:<%= class_name.underscore.pluralize %> => @<%= class_name.underscore.pluralize %>}
       else
-        render :json => { :errors => @<%= class_name.downcase.pluralize %>.errors.full_messages }, :status => 422
+        render :json => { :errors => @<%= class_name.underscore.pluralize %>.errors.full_messages }, :status => 422
       end
     else
-      render :json => { :errors => @<%= class_name.downcase.pluralize %>.errors.full_messages }, :status => 422
+      render :json => { :errors => @<%= class_name.underscore.pluralize %>.errors.full_messages }, :status => 422
     end
   end
 
